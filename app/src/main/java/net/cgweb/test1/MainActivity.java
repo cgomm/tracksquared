4737a059322b284d7f7695c22982003f527753c4
Felix <felixwortmann99@gmail.com>
2018-11-14 16:00:04 +0100

    Joining

--- app/src/main/java/net/cgweb/test1/MainActivity.java
+++ app/src/main/java/net/cgweb/test1/MainActivity.java
@@ -1,18 +1,30 @@
 package net.cgweb.test1;
 
-import android.arch.lifecycle.Lifecycle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinButton = findViewById(R.id.buttonJoining);
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                joiningButtonClicked(v);
+            }
+        });
     }
 
-    public void sumupTime(){
-
+    private void joiningButtonClicked(View view){
+        startActivity(new Intent(getApplicationContext(),AusUndEinsteigenActivity.class));
     }
 }
